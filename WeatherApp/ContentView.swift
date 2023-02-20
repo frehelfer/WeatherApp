6fdca8da7684971ccfb8ebceec3550911e1fbb05
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-20 20:00:57 -0300

    Init!

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            LinearGradient(colors: [Color.blue, Color.white], startPoint: .bottom, endPoint: .top)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+            }
         }
-        .padding()
     }
 }
 
