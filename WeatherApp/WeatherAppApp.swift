4fa30b1aa128896b90352f92b9c81f3a4b60da2e
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-20 18:43:33 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Frédéric Helfer on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
