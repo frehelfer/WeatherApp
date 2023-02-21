867620df83a86615620884a77506128bafbc20cb
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-21 13:39:01 -0300

    implement home ViewModel

---
+++ WeatherApp/Core/Home/HomeViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewModel.swift
+//  WeatherApp
+//
+//  Created by Frédéric Helfer on 21/02/23.
+//
+
+import Foundation
+
+@MainActor
+class HomeViewModel: ObservableObject {
+    @Published var currentWeather: CurrentWeather? = nil
+    @Published var forecastWeather: ForecastWeather? = nil
+    
+    private var weatherService = WeatherDataService()
+    
+    init() {
+        
+    }
+    
+    func fetchData() async {
+        do {
+            currentWeather = try await weatherService.fetchCurrentWeather(location: Location(lat: -29.737030, lon: -52.448650))
+            forecastWeather = try await weatherService.fetchForecastWeather(location: Location(lat: -29.737030, lon: -52.448650))
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
