cecd22239fe8a900e4a24ccbacd4d61cdaa6373c
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-21 12:43:18 -0300

    Implement Weather EndPoint

---
+++ WeatherApp/APIClient/WeatherEndpoint.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherEndpoint.swift
+//  WeatherApp
+//
+//  Created by Frédéric Helfer on 21/02/23.
+//
+
+import Foundation
+
+/// Represents unique API endpoint
+enum WeatherEndpoint: String {
+    case weather
+    case forecast
+}
